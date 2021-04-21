ee0ba23bb71a8019b430df239c72ae3e1fcf4ad2
jiawei-yu-97 <xaviyu@hotmail.com>
2021-04-21 00:18:59 -0400

    upload mini dataset

--- YOLOv3/test.py
+++ YOLOv3/test.py
@@ -160,35 +160,3 @@ def _create_validation_data_loader(img_path, batch_size, img_size, n_cpu):
         collate_fn=dataset.collate_fn)
     return dataloader
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Evaluate validation data.")
-    parser.add_argument("-m", "--model", type=str, default="config/yolov3.cfg", help="Path to model definition file (.cfg)")
-    parser.add_argument("-w", "--weights", type=str, default="weights/yolov3.weights", help="Path to weights or checkpoint file (.weights or .pth)")
-    parser.add_argument("-d", "--data", type=str, default="config/coco.data", help="Path to data config file (.data)")
-    parser.add_argument("-b", "--batch_size", type=int, default=8, help="Size of each image batch")
-    parser.add_argument("-v", "--verbose", action='store_true', help="Makes the validation more verbose")
-    parser.add_argument("--img_size", type=int, default=416, help="Size of each image dimension for yolo")
-    parser.add_argument("--n_cpu", type=int, default=8, help="Number of cpu threads to use during batch generation")
-    parser.add_argument("--iou_thres", type=float, default=0.5, help="IOU threshold required to qualify as detected")
-    parser.add_argument("--conf_thres", type=float, default=0.5, help="Object confidence threshold")
-    parser.add_argument("--nms_thres", type=float, default=0.5, help="IOU threshold for non-maximum suppression")
-    args = parser.parse_args()
-    print(args)
-
-    # Load configuration from data file
-    data_config = parse_data_config(args.data)
-    valid_path = data_config["valid"]  # Path to file containing all images for validation
-    class_names = load_classes(data_config["names"])  # List of class names
-
-    precision, recall, AP, f1, ap_class = evaluate_model_file(
-        args.model,
-        args.weights,
-        valid_path,
-        class_names,
-        batch_size=args.batch_size,
-        img_size=args.img_size,
-        n_cpu=args.n_cpu,
-        iou_thres=args.iou_thres,
-        conf_thres=args.conf_thres,
-        nms_thres=args.nms_thres,
-        verbose=True)
